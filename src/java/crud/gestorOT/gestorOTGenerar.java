80ec7ebc5d2936e982096a5847abab96229c6472
Games <Games@DESKTOP-OGCHKN9>
2018-11-20 01:55:29 -0300

    Se cambian paquetes.

--- src/java/crud/gestorOT/gestorOTGenerar.java
+++ src/java/crud/gestorOT/gestorOTGenerar.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package crud.gestorOT;
 
 import bd.ConexionBD;
@@ -10,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,22 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Lechuga
- */
 @WebServlet(name = "gestorOTGenerar", urlPatterns = {"/gestorOTGenerar"})
 public class gestorOTGenerar extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -40,16 +23,17 @@ public class gestorOTGenerar extends HttpServlet {
             
              HttpSession hs = request.getSession(false);
              
-            String idEmpresaAux = "" + hs.getAttribute("idEmpresa");
-            int idEmpresa = Integer.parseInt(idEmpresaAux);
+            String idEmpresa = "" + hs.getAttribute("idEmpresa");
             String importancia = request.getParameter("importancia");
             String idSupervisor = request.getParameter("idSupervisor");
             String nombreOT = request.getParameter("nombreOT");
             String detalleOT = request.getParameter("detalleOT");
             
-            String idProcedimientoAux = request.getParameter("idProcedimiento");
-            int idProcedimiento = Integer.parseInt(idProcedimientoAux);
+            String idProcedimiento = request.getParameter("idProcedimiento");
             
+            if (idProcedimiento == null){
+                idProcedimiento = "00000000000";
+            }
             String estado = "1";
 
             try {
@@ -61,8 +45,8 @@ public class gestorOTGenerar extends HttpServlet {
                 pst.setString(3, nombreOT);
                 pst.setString(4, detalleOT);
                 pst.setString(5, estado);
-                pst.setInt(6, idEmpresa);
-                pst.setInt(7, idProcedimiento);
+                pst.setString(6, idEmpresa); 
+                pst.setString(7, idProcedimiento);
                 pst.execute();
                 if (hs.getAttribute("tipoCuenta").equals("Administrador")) {
                     response.sendRedirect("/aeLita/administrador/gestorOT.jsp");
@@ -70,7 +54,7 @@ public class gestorOTGenerar extends HttpServlet {
                 if (hs.getAttribute("tipoCuenta").equals("Supervisor")) {
                     response.sendRedirect("/aeLita/supervisor/gestorOT.jsp");
                 }
-            } catch (Exception e) {
+            } catch (IOException | SQLException e) {
                 out.println("Excepción de SQL (RegistroUsuario.jsp): " + e);
             }
         }
