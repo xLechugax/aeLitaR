25140823bd09259e5d96db39b6ca06be88479965
Games <Games@DESKTOP-OGCHKN9>
2018-11-20 01:14:19 -0300

    Se cambian paquetes.

--- src/java/crud/gestorOT/gestorOTGenerar.java
+++ src/java/crud/gestorOT/gestorOTGenerar.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package crud;
+package crud.gestorOT;
 
 import bd.ConexionBD;
 import java.io.IOException;
