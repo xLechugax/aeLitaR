b74bb740e7902f0fde2da1734ea14ff88438c95c
Lechuga <Lechuga@10.58.5.45>
2018-12-15 16:08:19 -0300

    Bug min FIX

--- src/java/bd/ConexionBD.java
+++ src/java/bd/ConexionBD.java
@@ -1,25 +1,23 @@
 package bd;
+
 import java.sql.*;
 
-public class ConexionBD 
-{
+public class ConexionBD {
+
     private static String url = "jdbc:mysql://127.0.0.1:3306/aelita";
-    private static String user= "root";
-    private static String pass= "Teleco123";
+    private static String user = "root";
+    private static String pass = "Teleco123";
     private static Connection conn = null;
-     
+
     public static Connection getConexion() {
         try {
-            if(conn==null) {
-                Class.forName("com.mysql.jdbc.Driver");
-                conn = DriverManager.getConnection(url,user,pass);
-            }
-
-            return conn;
-        }
-        catch(Exception e) {
-            System.err.println("Excepción de Conexión de SQL: " + e);
-            return null;
+            Class.forName("com.mysql.jdbc.Driver");
+            String unicode = "useSSL=false&autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8";
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/aelita?" + unicode, user, pass);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("couldn't connect!");
+            throw new RuntimeException(ex);
         }
     }
 }
